dcc310260f17e3e2478f758fb14888cbe2965572
Kassandra Salazar <14470225@itcampeche.edu.mx>
2017-10-26 11:42:40 -0500

    Versión inicial del proyecto ImcApp. No incluye manejo de eventos.

---
+++ app/src/main/java/imcapp/kassandrasalazar/android/imcapp/ImcFragment.java
@@ -0,0 +1,26 @@
+package imcapp.kassandrasalazar.android.imcapp;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by KASSANDRA-PC on 26/10/2017.
+ */
+
+public class ImcFragment extends Fragment{
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragmento_imc, container, false);
+        return v;
+    }
+}
